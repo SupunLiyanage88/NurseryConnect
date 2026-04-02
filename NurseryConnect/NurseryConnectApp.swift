a2722669419e960d73a3121ba9d7d149d68c73cc
Supun Liyanage <liyanagesupun10@gmail.com>
2026-04-03 02:45:51 +0530

    first commit

---
+++ NurseryConnect/NurseryConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurseryConnectApp.swift
+//  NurseryConnect
+//
+//  Created by Supun Liyanage on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct NurseryConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
